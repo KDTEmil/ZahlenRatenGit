1c15cb2bed3a229dbdc54ce86e8cfe869f37ebe9
KDTEmil <noxpatronus1123@gmail.com>
2025-05-08 10:28:38 +0200

    game code created

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
 # Main Project File
+
+from random import randint as r
+
+geheimzahl = r(1, 100)
+versuche = 0
+print("Willkommen zum Zahlenratespiel!")
+print("Ich habe mir eine Zahl zwischen 1 und 100 ausgedacht. Kannst du sie erraten?")
+
+while True:
+    tipp = int(input("Gib deinen Tipp ein: "))
+    versuche += 1
+    if tipp < geheimzahl:
+        print("Zu niedrig!")
+    elif tipp > geheimzahl:
+        print("Zu hoch!")
+    else:
+        print(f"Richtig! Du hast die Zahl in {versuche} Versuchen erraten.")
+        break
+
+
