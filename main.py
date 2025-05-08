a5d23112aababb7fa3b06b882da01f4aa9602795
KDTEmil <noxpatronus1123@gmail.com>
2025-05-08 10:40:44 +0200

    Added functions to main.py

--- main.py
+++ main.py
@@ -2,20 +2,24 @@
 
 from random import randint as r
 
-geheimzahl = r(1, 100)
-versuche = 0
-print("Willkommen zum Zahlenratespiel!")
-print("Ich habe mir eine Zahl zwischen 1 und 100 ausgedacht. Kannst du sie erraten?")
+def zahlenratespiel():
+    geheimzahl = r(1, 100)
+    versuche = 0
+    print("Willkommen zum Zahlenratespiel!")
+    print("Ich habe mir eine Zahl zwischen 1 und 100 ausgedacht. Kannst du sie erraten?")
 
-while True:
-    tipp = int(input("Gib deinen Tipp ein: "))
-    versuche += 1
-    if tipp < geheimzahl:
-        print("Zu niedrig!")
-    elif tipp > geheimzahl:
-        print("Zu hoch!")
-    else:
-        print(f"Richtig! Du hast die Zahl in {versuche} Versuchen erraten.")
-        break
+    while True:
+        try:
+            tipp = int(input("Gib deinen Tipp ein: "))
+            versuche += 1
+            if tipp < geheimzahl:
+                print("Zu niedrig!")
+            elif tipp > geheimzahl:
+                print("Zu hoch!")
+            else:
+                print(f"Richtig! Du hast die Zahl in {versuche} Versuchen erraten.")
+                break
+        except ValueError:
+            print("Bitte gib eine gültige Zahl ein.")
 
-
+zahlenratespiel()
